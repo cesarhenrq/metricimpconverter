235f69e6558b570200e3c9aa9c610c33a4e59aec
Cesar Henrique <cesaarhvb@gmail.com>
2025-05-31 19:00:15 -0300

    feat(api): add conversion endpoint to process input and return conversion results

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,22 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.route("/api/convert").get(function (req, res) {
+    const input = req.query.input;
+    const { initNum, initUnit, returnNum, returnUnit, string } =
+      convertHandler.processInput(input);
+
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string,
+    });
+  });
 };
