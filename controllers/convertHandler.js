6921a8c14ee2883c27ba9e1f4a38e5ac639f4eed
Cesar Henrique <cesaarhvb@gmail.com>
2025-06-03 00:38:38 -0300

    refactor(ConvertHandler): streamline number and unit handling, improve validation, and enhance conversion logic

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,9 +1,15 @@
+const { roundToFiveDecimals } = require("../utils");
+
 function ConvertHandler() {
   this.getNum = function (input) {
-    this.validateInput(input);
-
     const inputWithoutUnit = this.removeUnit(input);
-    let result;
+
+    if (
+      inputWithoutUnit.includes("/") &&
+      inputWithoutUnit.split("/").length > 2
+    ) {
+      return "invalid number";
+    }
 
     if (inputWithoutUnit === "") {
       return 1;
@@ -11,136 +17,79 @@ function ConvertHandler() {
 
     if (inputWithoutUnit.includes("/")) {
       const [num, denom] = inputWithoutUnit.split("/");
-      result = num / denom;
-    } else {
-      result = parseFloat(inputWithoutUnit);
+      return parseFloat(num) / parseFloat(denom);
     }
 
-    return result;
+    return parseFloat(inputWithoutUnit);
   };
 
   this.getUnit = function (input) {
-    let result;
+    const match = input.match(/[a-zA-Z]+$/);
+    if (!match) return "invalid unit";
 
-    const allowedUnits = ["L", "gal", "lbs", "kg", "mi", "km"];
+    const unit = match[0];
+    const unitLower = unit.toLowerCase();
+    const validUnits = ["gal", "l", "mi", "km", "lbs", "kg"];
 
-    const includeUnit = (unit) => input.includes(unit);
-
-    if (!allowedUnits.some(includeUnit)) {
-      throw new Error("invalid unit");
+    if (!validUnits.includes(unitLower)) {
+      return "invalid unit";
     }
 
-    if (includeUnit("L")) {
-      result = "L";
-    } else if (includeUnit("gal")) {
-      result = "gal";
-    } else if (includeUnit("lbs")) {
-      result = "lbs";
-    } else if (includeUnit("kg")) {
-      result = "kg";
-    } else if (includeUnit("mi")) {
-      result = "mi";
-    } else if (includeUnit("km")) {
-      result = "km";
-    }
-
-    return result;
+    return unitLower === "l" ? "L" : unitLower;
   };
 
   this.getReturnUnit = function (initUnit) {
-    let result;
-
     const unitMap = {
-      L: "gal",
       gal: "L",
-      lbs: "kg",
-      kg: "lbs",
+      L: "gal",
       mi: "km",
       km: "mi",
+      lbs: "kg",
+      kg: "lbs",
     };
 
-    result = unitMap[initUnit] || "invalid unit";
-
-    return result;
+    return unitMap[initUnit];
   };
 
   this.spellOutUnit = function (unit) {
-    let result;
-
     const unitMap = {
-      L: "liters",
       gal: "gallons",
-      lbs: "pounds",
-      kg: "kilograms",
+      L: "liters",
       mi: "miles",
       km: "kilometers",
+      lbs: "pounds",
+      kg: "kilograms",
     };
 
-    result = unitMap[unit] || "invalid unit";
-
-    return result;
+    return unitMap[unit];
   };
 
   this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
 
     const unitMap = {
-      L: galToL,
-      gal: 1 / galToL,
-      lbs: lbsToKg,
-      kg: 1 / lbsToKg,
-      mi: miToKm,
-      km: 1 / miToKm,
+      gal: initNum * galToL,
+      L: initNum / galToL,
+      lbs: initNum * lbsToKg,
+      kg: initNum / lbsToKg,
+      mi: initNum * miToKm,
+      km: initNum / miToKm,
     };
 
-    result = initNum * unitMap[initUnit] || "invalid unit";
-
-    return result;
+    return roundToFiveDecimals(unitMap[initUnit]);
   };
 
   this.getString = function (initNum, initUnit, returnNum, returnUnit) {
-    let result;
-
-    const spellOutUnit = this.spellOutUnit(initUnit);
+    const spellOutInitUnit = this.spellOutUnit(initUnit);
     const spellOutReturnUnit = this.spellOutUnit(returnUnit);
 
-    result = `${initNum} ${spellOutUnit} converts to ${returnNum} ${spellOutReturnUnit}`;
-
-    return result;
-  };
-
-  this.processInput = function (input) {
-    const initNum = this.getNum(input);
-    const initUnit = this.getUnit(input);
-    const returnNum = this.convert(initNum, initUnit);
-    const returnUnit = this.getReturnUnit(initUnit);
-    const string = this.getString(initNum, initUnit, returnNum, returnUnit);
-
-    return {
-      initNum,
-      initUnit,
-      returnNum,
-      returnUnit,
-      string,
-    };
+    return `${initNum} ${spellOutInitUnit} converts to ${returnNum} ${spellOutReturnUnit}`;
   };
 
   this.removeUnit = function (input) {
     return input.replace(/[a-zA-Z]/g, "");
-  };
-
-  this.validateInput = function (input) {
-    const inputWithoutUnit = this.removeUnit(input);
-
-    if (
-      inputWithoutUnit.includes("/") &&
-      inputWithoutUnit.split("/").length > 2
-    ) {
-      throw new Error("invalid number");
-    }
   };
 }
 
